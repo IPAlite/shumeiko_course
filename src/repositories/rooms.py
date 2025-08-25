0ffca2057289444a799b633ec50d3409b62f3d61
IPAlite <93054603+IPAlite@users.noreply.github.com>
2025-08-26 00:22:09 +0300

    task17#_my_solve

--- src/repositories/rooms.py
+++ src/repositories/rooms.py
@@ -1,10 +1,11 @@
 from datetime import date
 
 from sqlalchemy import select, func
+from sqlalchemy.orm import selectinload, joinedload
 
 from src.repositories.base import BaseRepository
 from src.models.rooms import RoomsOrm
-from src.schemas.rooms import Room
+from src.schemas.rooms import Room, RoomsWithRels
 from src.models.bookings import BookingsOrm
 from src.database import engine
 from src.repositories.utils import rooms_ids_for_booking
@@ -15,4 +16,21 @@ class RoomsRepository(BaseRepository):
 
     async def get_filtered_by_time(self, hotel_id:int, date_from: date, date_to: date):
         rooms_ids_to_get = rooms_ids_for_booking(hotel_id=hotel_id, date_from=date_from, date_to=date_to)
-        return await self.get_filtered(RoomsOrm.id.in_(rooms_ids_to_get.scalar_subquery()))
+
+        query = (
+            select(self.model)
+            .options(joinedload(self.model.facilities))
+            .filter(RoomsOrm.id.in_(rooms_ids_to_get))
+        )
+        result = await self.session.execute(query)
+        return [RoomsWithRels.model_validate(model) for model in result.unique().scalars().all()]
+    
+
+    async def get_one_or_none(self, **filter_by):
+        query = (
+            select(self.model)
+            .filter_by(**filter_by)
+            .options(joinedload(self.model.facilities))
+        )
+        result = await self.session.execute(query)
+        return [RoomsWithRels.model_validate(model) for model in result.unique().scalars().all()]
