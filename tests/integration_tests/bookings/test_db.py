8fa01ea946491552d643422eb93aefc1d928b94e
IPAlite <93054603+IPAlite@users.noreply.github.com>
2025-09-05 13:59:04 +0300

    task#20_atrem_solve

--- tests/integration_tests/bookings/test_db.py
+++ tests/integration_tests/bookings/test_db.py
@@ -17,20 +17,25 @@ async def test_booking_crud(db):
         date_to = date(year=2025, month=9, day=5),
         price = 500
     )
-    await db.bookings.add(booking_data)
+    new_booking = await db.bookings.add(booking_data)
 
     # read
-    exists_booking = await db.bookings.get_filtered(user_id=user_id)
-    assert exists_booking
-    
+    booking = await db.bookings.get_one_or_none(id=new_booking.id)
+    assert booking
+    assert booking.id == new_booking.id
+    assert booking.room_id == new_booking.room_id
+    assert booking.user_id == new_booking.user_id
+
     # update
     booking_data = BookingPatch(
         price=15000
     )
-    await db.bookings.edit(booking_data, exclude_unset=True)
+    await db.bookings.edit(booking_data, id=booking.id, exclude_unset=True)
+    updated_booking = await db.bookings.get_one_or_none(id=booking.id)
+    assert updated_booking
+    assert updated_booking.user_id == user_id
+    assert updated_booking.price != booking.price
 
     # delete
     booking_id = (await db.bookings.get_all())[0].id
     await db.bookings.delete(id=booking_id)
-
-    await db.commit()
